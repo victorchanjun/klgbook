0f1b8bed8e407073fe3fbb93168bdd0ccb5ac676
victorchanjun <black.and.white.freedom@hotmail.com>
2026-06-12 12:12:11 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'kallang-v3';
+const SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('perfectgym.com')) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
